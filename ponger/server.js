f3dfd55c1bafc2479f2e562d8e19bb490f343267
Maxwell Feldman <m.feldman@mfeldman-ltm.internal.salesforce.com>
2015-03-15 14:57:05 -0700

    updated server

--- ponger/server.js
+++ ponger/server.js
@@ -5,23 +5,23 @@ var io = socketio.listen(8080);
 var playerCount = 0;
 
 io.on('connection', function (socket) {
-    // player 0 sent a message, route it to player 1
+    // player 0 sent a message, broadcast it
     socket.on('0', function (data) {
         console.log('player 0');
         console.log(data);
-        // send on channel 1
-        io.sockets.emit('1', data);
+        // send as msg from player 0
+        io.sockets.emit('0', data);
         // TODO: keep a socket on a certain channel for perf reasons
         //socket.join('0');
         //io.to('1').emit('1', data);
     });
 
-    // handle a message from player 1
+    // handle a message from player 1, broadcast to all
     socket.on('1', function (data) {
         console.log('player 1');
         console.log(data);
-        // send on channel 0
-        io.sockets.emit('0', data);
+        // send as msg from player 1 
+        io.sockets.emit('1', data);
         // TODO: keep a socket on a certain channel for perf reasons
         //socket.join('1');
         //io.to('0').emit('0', data);
